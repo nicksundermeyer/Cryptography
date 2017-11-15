7eff17ef52345d35fe8a4a224c74e5b7fc011a5f
Nicholas Sundermeyer <nicsunde@cs.du.edu>
2017-11-15 17:52:07 +0100

    modified quadratic sieve

--- QuadraticSieve.py
+++ QuadraticSieve.py
@@ -1,11 +1,58 @@
+import subprocess
+
 # Basic Quadratic Sieve Function 
 # Works for small numbers
 
 # Version 1.0
 # O(N^2)
 def basicQuadraticSieve( N ):
-	for x in range(0, N)
-		for y in range(0, N)
-			if ((x - y)*(x + y) % N) == 0
-				print(x-y)
-				print(x+y)
+	print(N)
+	for x in range(1, N):
+		for y in range(1, N):
+			if ( (x * x) == ( (y * y) % N) and x != y): 
+					a = x+y
+					b = N
+
+					print('initials')
+					print (a)
+					print (b)
+					# Gets the gcd of N and x+y
+					while ( 1 ):
+						# Gets q given p which is gcd.
+						if ( a == 0 ):
+							p = b
+							q = N/b
+							print ('p is ' + str(p) )
+							print ('q is ' + str(q) )
+							return
+						c = b % a
+						b = a
+						a = c
+						
+# Testing code
+# basicQuadraticSieve(457 * 673)
+
+# Add parameters later for matrix input 
+def createMatrixInput ():
+	# Matrix Dimensions with dummy values
+	M = 10
+	N = 10
+
+	# Matrix to
+	matrix = [ [ (x-y) % 2 for x in range(M) ] for y in range(N) ]
+	print(str(matrix))
+	line1 = [ 0, 1, 2, 3, 4 ]
+	file  = open("matrixInput.txt", "w")
+
+	file.write(str(M) + " " + str(N) + "\n")
+	for line in matrix:
+		for item in line:
+			file.write( "%s "  % item )
+		file.write("\n")
+	file.close()
+	return
+
+createMatrixInput()
+
+def GaussianElimination ( filename ):
+	
